0302c13293522ec4f6765ea2550c31449d456b14
Selivanova Anna <foxy.eyed@gmail.com>
2016-08-18 12:59:30 +0400

    Add validation to controller actions and improve methods for train and station creation

--- Lesson_04/controller.rb
+++ Lesson_04/controller.rb
@@ -27,6 +27,8 @@ class Controller
       create_station
     when "2"
       create_train
+    when "3"
+      attach_wagon
     when "6"
       list_stations
     else
@@ -38,31 +40,80 @@ class Controller
 
   private
 
+  # action #1
   def create_station
-    print "Укажите название станции: "
-    station = Station.new(gets.chomp)
-    @stations << station
-    puts "Станция «#{station}» создана."
+    loop do
+      print "Укажите название станции: "
+      name = gets.chomp
+
+      check = validate_station(name)
+      if check[:success]
+        create_station!(name)
+        break
+      else
+        puts check[:errors]
+      end
+    end
   end
 
+  # action #2
   def create_train
     loop do
       print "Введите номер поезда: "
       number = gets.chomp
       print "Укажите тип поезда ([1] > грузовой, [2] > пассажирский): "
       type = gets.chomp.to_i
-      if type == 1
-        train = CargoTrain.new(number)
-      elsif type == 2
-        train = PassengerTrain.new(number)
+      
+      check = validate_train(type, number)
+      if check[:success]
+        create_train!(type, number)
+        break
+      else
+        puts check[:errors]
       end
-      break if !train.nil?
     end
   end
 
+  # action #3
+  def attach_wagon
+    # предложить пользователю выбрать поезд, введя название
+    # если поезд найден, опередлить его тип
+    # создать инстанс вагона того же типа
+    # прицепить
+    # показать обновленную инфу о поезде
+  end
+  
+  # action #6
   def list_stations
     puts "Станции в системе:"
     puts self.stations
+  end
+
+  def validate_train(type, number)
+    errors = []
+    valid_types = [1, 2]
+    errors << "Номер поезда не может быть пуст!" if number.empty?
+    errors << "Некорректный тип поезда!" if !valid_types.include?(type)
+    errors.empty? ? {success: true} : {success: false, 'errors': errors}
+  end
+
+  def validate_station(name)
+    errors = []
+    errors << "Название станции обязательно!" if name.empty?
+    errors.empty? ? {success: true} : {success: false, 'errors': errors}
+  end
+
+  def create_train!(type, number)
+    train = type == 1 ? CargoTrain.new(number) : PassengerTrain.new(number)
+    @trains << train
+    puts "#{train} создан."
+    puts train.info
+  end
+
+  def create_station!(name)
+    station = Station.new(name)
+    @stations << station
+    puts "Станция «#{station}» создана."
   end
   
 end
