3e803b1ae0df0949de911f93b538634e9c917da3
Selivanova Anna <foxy.eyed@gmail.com>
2016-08-31 22:09:03 +0400

    Change structure storing class validation rules

--- Lesson_09/modules/validation.rb
+++ Lesson_09/modules/validation.rb
@@ -6,8 +6,8 @@ module Validation
 
   module ClassMethods
     def validate(name, validator, options = nil)
-      key = "#{name}_#{validator}".to_sym
-      rules[key] = { validator: validator, args: [name, options].compact }
+      rules[name] = rules.key?(name) ? rules[name].dup : {}
+      rules[name][validator] = [options].compact
     end
 
     def rules
@@ -29,25 +29,25 @@ module Validation
     protected
 
     def validate!
-      self.class.rules.each do |_key, rule|
-        value = instance_variable_get("@#{rule[:args].first}")
-        send(rule[:validator], value, *rule[:args])
+      self.class.rules.each do |name, rules|
+        value = instance_variable_get("@#{name}")
+        rules.each { |validator, options| send(validator, name, value, *options) }
       end
     end
 
-    def presence(val, name)
+    def presence(name, val)
       raise "Атрибут #{name} обязателен!" if val.nil? || val.empty?
     end
 
-    def format(val, name, pattern)
+    def format(name, val, pattern)
       raise "Недопустимый формат #{name}!" if pattern.match(val).nil?
     end
 
-    def type(val, name, type)
+    def type(name, val, type)
       raise "Атрибут #{name} должен иметь тип #{type}!" unless val.is_a?(type)
     end
 
-    def positive_number(val, name)
+    def positive_number(name, val)
       raise "Атрибут #{name} должен быть больше нуля!" unless val > 0
     end
   end
